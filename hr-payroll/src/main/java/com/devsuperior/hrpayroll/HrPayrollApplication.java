d3d0b07602f34e56b928a13cde32f37dc8540f07
Nelio Alves <acenelio@gmail.com>
2020-10-11 16:40:24 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollApplication.class, args);
+	}
+
+}
---
+++ hr-payroll/src/test/java/com/devsuperior/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
