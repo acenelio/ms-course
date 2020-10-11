a73fa4a7c42e1112c326b0647aa8c775051dc550
Nelio Alves <acenelio@gmail.com>
2020-10-11 15:37:18 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
