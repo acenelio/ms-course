25a5a545f6f832d57239dfa7feb8ebd121320904
Nelio Alves <acenelio@gmail.com>
2020-10-12 22:47:34 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
