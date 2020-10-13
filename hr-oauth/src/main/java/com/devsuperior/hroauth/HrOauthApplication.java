f8cc6c5dfba0ac4b2ee75c12d2a26559e366f98f
Nelio Alves <acenelio@gmail.com>
2020-10-13 18:13:15 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/HrOauthApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+
+}
