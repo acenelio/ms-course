0fb9685017fbf048ecd903bf34c129ff95dd57d3
Nelio Alves <acenelio@gmail.com>
2020-10-13 14:08:53 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
---
+++ hr-user/src/test/java/com/devsuperior/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
