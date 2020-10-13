0cc5b6212f8b8084bc67f9efa83a7dbe848cdafa
Nelio Alves <acenelio@gmail.com>
2020-10-13 00:48:44 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,24 @@
 package com.devsuperior.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("USERNAME = " + username);
+	}
 }
